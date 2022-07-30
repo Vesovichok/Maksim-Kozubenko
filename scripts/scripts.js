2f60cceeecd9bc331363387a00b4f7a11189e0a3
Maksim Kozubenko <manager.vesovichok@gmail.com>
2022-07-30 23:12:00 +0300

    tas1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,18 @@
-console.log(`Hello world`);
+     // 1 - Создать переменную каждого типа данных. //
+
+var number = 1;
+console.log(number)
+
+var str = 'name';
+console.log(str);
+
+var bool = true;
+console.log(bool)
+
+// 2 - Создать переменную var, let, const. Перезаписать их значения (не для const). //
+
+var number = 2;
+number = 2.3;
+console.log(number)
+
+
